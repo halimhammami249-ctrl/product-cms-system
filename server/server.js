b8a5ba5e2268b6873a5f16d9ec00ffb8d30c371e
halim hammami <halim.hammami249@gmail.com>
2026-06-20 12:11:11 +0100

    Prepare backend for deployment

--- server/server.js
+++ server/server.js
@@ -40,6 +40,8 @@ app.post('/api/:client/products', (req, res) => {
   res.json({ success: true });
 });
 
-app.listen(3001, () => {
-  console.log('Server running on http://localhost:3001');
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
